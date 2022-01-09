2d5ad98f48143477e4f923b5e47479c60969d561
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-01-09 15:36:49 -0500

    2.2 started

--- experiments/2.constants/js/script.js
+++ experiments/2.constants/js/script.js
@@ -9,26 +9,45 @@ author, and this description to match your project!
 "use strict";
 
 //2.1 constants
-const NUM_CIRCLES = 10;
-let circleAlpha = 50;
-let circleSizeIncrease = 50;
+// const NUM_CIRCLES = 10;
+// let circleAlpha = 50;
+// let circleSizeIncrease = 50;
+//
+//
+// function setup() {
+//   createCanvas(500, 500);
+// }
+//
+//
+// function draw() {
+//   background(0);
+//
+//   circleAlpha = map(mouseX, 0, width, 0, 100);
+//   circleSizeIncrease = map(mouseY, 0, height, 0, 100);
+//
+//   for (let i=0; i<NUM_CIRCLES; i++) {
+//     push();
+//     fill(255, circleAlpha);
+//     ellipse(width/2, height/2, i*circleSizeIncrease);
+//     pop();
+//   }
+// }
 
-
+//2.2 Object parameters
 function setup() {
   createCanvas(500, 500);
 }
 
-
 function draw() {
   background(0);
 
-  circleAlpha = map(mouseX, 0, width, 0, 100);
-  circleSizeIncrease = map(mouseY, 0, height, 0, 100);
+  drawFancyRect(250, 250, 200, 200, 255, 255, 0, CENTER);
+}
 
-  for (let i=0; i<NUM_CIRCLES; i++) {
-    push();
-    fill(255, circleAlpha);
-    ellipse(width/2, height/2, i*circleSizeIncrease);
-    pop();
-  }
+function drawFancyRect(x, y, w, h, r, g, b, mode) {
+  push();
+  fill();
+  rectMode();
+  rect();
+  pop();
 }
