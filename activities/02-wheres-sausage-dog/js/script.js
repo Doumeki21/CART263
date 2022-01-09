9c9515cbcee8fc78d97e84e854977eade875c235
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-01-09 17:19:55 -0500

    act2: shows animals

--- activities/02-wheres-sausage-dog/js/script.js
+++ activities/02-wheres-sausage-dog/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Activity2: Where's Sausage dog?
+Olenka Yuen
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -8,26 +8,39 @@ author, and this description to match your project!
 
 "use strict";
 
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
 
 /**
 Description of preload
 */
 function preload() {
-
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage); // adding animalImage into the array
+  }
 }
 
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  //create animals
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage); //create an animal based on the 3 codes above and the Animal.js
+    animals.push(animal); // adding animal into the animals array
+  }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(255, 255, 0);
 
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update(); //update the animal at position i.
+  }
 }
