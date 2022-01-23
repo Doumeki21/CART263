06b8ac6f69acd9b5e4911cdcc6833ac52b886749
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-01-23 18:12:18 -0500

    3.1 started

---
+++ experiments/3.voices/js/script.js
@@ -0,0 +1,29 @@
+/**
+ResponsiveVoice
+
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+
+function preload() {
+
+}
+
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+
+function draw() {
+  background(0);
+}
+
+function mousePressed() {
+  responsiveVoice.speak("what did he do?");
+}
