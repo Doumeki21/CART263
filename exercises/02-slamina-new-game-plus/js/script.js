b36c6a14abd856f5c319221b6d1b69fdafa0b85b
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-01-24 13:48:45 -0500

    EX2: still wip (win/lose texts don't show), code works

--- exercises/02-slamina-new-game-plus/js/script.js
+++ exercises/02-slamina-new-game-plus/js/script.js
@@ -14,155 +14,155 @@ REQUIREMENTS (do at least 3):
 
 "use strict";
 
-const animals =  [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
-    ];
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra",
+];
 
-    let currentAnimal = '';
-    let currentAnswer = '';
+let currentAnimal = "";
+let currentAnswer = "";
 
-    let winLetters = [`G`, `R`, `E`, `A`, `T`];
-    let loseLetters = [`W`, `R`, `O`, `N`, `G`];
+let winLetters = [`G`, `R`, `E`, `A`, `T`];
+let loseLetters = [`W`, `R`, `O`, `N`, `G`];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   if (annyang) {
     let commands = {
-      'I think it is *animal': guessAnimal
+      "I think it is *animal": guessAnimal,
     };
     annyang.addCommands(commands);
     annyang.start();
@@ -179,24 +179,30 @@ function draw() {
   if (currentAnswer === currentAnimal) {
     fill(0, 255, 0);
     createWinLetters();
-  }
-  else {
+  } else {
     fill(255, 0, 0);
   }
-  text(currentAnswer, width/2, height/2);
+  text(currentAnswer, width / 2, height / 2);
 }
 
 function createWinLetters() {
   // A loop to add all letters.
   for (let i = 0; i < winLetters.length; i++) {
     // Generate a random x on the canvas
-    let x = random(width/2 - 100, width/2 + 100);
+    let x = random(width / 2 - 100, width / 2 + 100);
     // Generate a random y ABOVE the canvas (so they fall in)
     let y = random(-400, -100);
     // // Create the letter
     // let winLetter = new WinLetter(x, y, `G`);
     // // Add the new winLetter to our winLetters array
     // winLetters.push(winLetter);
+    push();
+    fill(255);
+    noStroke();
+    textSize(32);
+    textStyle(BOLD);
+    pop();
+  }
 }
 
 function mousePressed() {
@@ -215,11 +221,11 @@ Reverses the provided string
 */
 function reverseString(string) {
   // Split the string into an array of characters
-  let characters = string.split('');
+  let characters = string.split("");
   // Reverse the array of characters
   let reverseCharacters = characters.reverse();
   // Join the array of characters back into a string
-  let result = reverseCharacters.join('');
+  let result = reverseCharacters.join("");
   // Return the result
   return result;
 }
