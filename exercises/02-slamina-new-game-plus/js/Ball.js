83972e4863f8810f2f03a4012f0f1ce0fe2b02d2
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-01-25 14:01:15 -0500

    EX2: (actual) CREATED BALL.JS wip

---
+++ exercises/02-slamina-new-game-plus/js/Ball.js
@@ -0,0 +1,18 @@
+class Ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.ax = 0;
+    this.ay = 0;
+    this.maxSpeed = 10;
+    this.size = 40;
+    this.active = true;
+  }
+
+  //Ball goes down due to gravity.
+  gravity(force) {
+    this.ay += force;
+  }
+}
