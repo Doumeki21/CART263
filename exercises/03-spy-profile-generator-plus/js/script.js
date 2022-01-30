e2494cc82abc2f11fe308e1d934b339c20361582
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-01-30 00:06:04 -0500

    EX3: generator works except the color/ hue

--- exercises/03-spy-profile-generator-plus/js/script.js
+++ exercises/03-spy-profile-generator-plus/js/script.js
@@ -13,75 +13,99 @@ Add at least 3 of the following:
 
 let characterProfile = {
   name: `N/A`,
+  hue: `N/A`,
   nature: `N/A`,
   secretWeapon: `N/A`,
   password: `N/A`,
 };
 
-let instrumentData = undefined;
-let objectData = undefined;
+let hueData = undefined;
+let descriptionData = undefined;
+let pastryData = undefined;
 let tarotData = undefined;
 
 function preload() {
-  instrumentData = loadJSON(
-    `https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`
-  );
-
-  objectData = loadJSON(
-    `https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`
-  );
-
-  tarotData = loadJSON(
-    `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`
-  );
+  hueData = loadJSON(`assets/data/web_colors.json`);
+  descriptionData = loadJSON(`assets/data/descriptions.json`);
+  pastryData = loadJSON(`assets/data/breads_and_pastries.json`);
+  tarotData = loadJSON(`assets/data/tarot_interpretations.json`);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+  let data = JSON.parse(localStorage.getItem(`character-profile-data`));
 
   if (data !== null) {
-    let password = prompt(`Agent! What is your password?`);
+    let password = prompt(`Password please!`);
     if (password === data.password) {
-      spyProfile.name = data.name;
-      spyProfile.alias = data.alias;
-      spyProfile.secretWeapon = data.secretWeapon;
-      spyProfile.password = data.password;
+      characterProfile.name = data.name;
+      characterProfile.hue = data.hue;
+      characterProfile.nature = data.nature;
+      characterProfile.secretWeapon = data.secretWeapon;
+      characterProfile.password = data.password;
     }
   } else {
-    generateSpyProfile();
+    generateCharacterProfile();
   }
 }
 
-function generateSpyProfile() {
-  spyProfile.name = prompt(`Agent! What is your name?`);
+function generateCharacterProfile() {
+  characterProfile.name = prompt(`Sup! What's your character name?`);
 
-  let instrument = random(instrumentData.instruments);
-  spyProfile.alias = `The ${instrument}`;
-  spyProfile.secretWeapon = random(objectData.objects);
+  let chosenColor = random(hueData.colors);
+  characterProfile.hue = random(chosenColor.color);
+
+  characterProfile.nature = random(descriptionData.descriptions);
+  characterProfile.secretWeapon = random(pastryData.pastries);
 
   let card = random(tarotData.tarot_interpretations);
-  spyProfile.password = random(card.keywords);
+  characterProfile.password = random(card.keywords);
 
-  localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
+  localStorage.setItem(
+    `character-profile-data`,
+    JSON.stringify(characterProfile)
+  );
 }
 
 function draw() {
   background(255);
 
-  let profile = `** SPY PROFILE! DO NOT DISTRIBUTE! **
+  let profile = `** CREATE YOUR OWN CHARACTER! **
 
-  Name: ${spyProfile.name}
-  Alias: ${spyProfile.alias}
-  Secret Weapon: ${spyProfile.secretWeapon}
-  Password: ${spyProfile.password}`;
+  Name: ${characterProfile.name}
+  Hue: ${characterProfile.hue}
+  Nature: ${characterProfile.nature}
+  Secret Weapon: ${characterProfile.secretWeapon}
+  Password: ${characterProfile.password}`;
 
+  //display the text as the indicated profile variable above.
   push();
   textFont(`Courier, monospace`);
   textSize(24);
-  textAlign(LEFT, TOP);
+  textAlign(CENTER, CENTER);
   fill(0);
-  text(profile, 100, 100);
+  text(profile, width / 2, height / 2);
   pop();
+
+  //display instructions to clear data
+  push();
+  textFont(`Courier, monospace`);
+  textSize(24);
+  textAlign(CENTER, CENTER);
+  textStyle(BOLD);
+  fill(0);
+  text(
+    `REMEMBER YOUR PASSWORD! \n OR \n Press C to create a new character before closing the browser.`,
+    width / 2,
+    height - 100
+  );
+  pop();
+}
+
+//pess C on keyboard and reload the broswer to clear data
+function keyPressed() {
+  if (key === `c`) {
+    localStorage.removeItem(`character-profile-data`);
+  }
 }
