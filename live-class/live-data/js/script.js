a2fdb655560313a3ff0b6e3a8294b2fb9edcf692
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-01 14:48:42 -0500

    live: data (feeling lost)

--- live-class/live-data/js/script.js
+++ live-class/live-data/js/script.js
@@ -1,33 +1,96 @@
 /**
-Title of Project
-Author Name
+live: data
+Olenka Yuen
+February 1, 2022
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+localStorage
+  *how to store more than 1 data-set
+  *How to trigger a new save/ item
+  *what about saving files to the pc itself
+*JSON
+  *How do you go through a JSON array sequentially
+  *creating a character dialog (Q&A).
+
 */
 
 "use strict";
 
+let saveData = {
+  key: "cart263a-data-live-coding-save-data",
+  players: [
+    {
+      name: "Joachim",
+      age: 23,
+      eyeColor: "turquoise",
+      background: {
+        r: 255,
+        g: 200,
+        b: 120,
+      },
+    },
+    {
+      name: "John",
+      age: 20,
+      eyeColor: "diamond",
+      background: {
+        r: 255,
+        g: 100,
+        b: 0,
+      },
+    },
+  ],
+  dialog: {
+    scene1: [
+      {
+        character: "Joachim",
+        text: "Do you know where she is?",
+      },
+      {
+        character: "John",
+        text: "She might be at the library.",
+      },
+      {
+        character: "Joachim",
+        text: "Thanks I'll go check.",
+      },
+    ],
+  },
+};
 
-/**
-Description of preload
-*/
-function preload() {
+function preload() {}
 
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(500, 500);
 
+  let loadData = JSON.parse(localStorage.getItem(saveData.key));
+  if (loadData !== undefined) {
+    saveData = loadData;
+  }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+  background(0);
 
+  for (let i = 0; i < saveData.players.length; i++) {
+    let player = saveData.players[i];
+
+    push();
+    fill(player.background.r, player.background.g, player.background.b);
+    rect(0, i * 100, 200, 100);
+    text(player.name, 0, i * 100 + 100);
+    pop();
+  }
+}
+
+function mousePressed() {
+  let player = {
+    name: random(["Frank", "Steve", "Fiona"]),
+    age: random(10, 80),
+    eyeColor: random(["Red", "Blue"]),
+    background: {
+      r: (100, 200),
+      g: (100, 200),
+      b: (100, 200),
+    },
+  };
 }
