4c8eff01ec3de232f965d84a892ef99e19b331c9
OlenkaYuen <noiz1991yuya@gmail.com>
2022-02-15 15:39:52 -0500

    p1 draft: purple enemy moves correctly

--- sandbox/project-1-brainstorm/js/PurpleEnemy.js
+++ sandbox/project-1-brainstorm/js/PurpleEnemy.js
@@ -7,42 +7,46 @@ class PurpleEnemy {
     this.vy = 0;
     this.speed = 10;
     this.angle = 0;
-    this.rotatingSpeed = 10;
+    this.rotatingSpeed = 2;
 
-    this.pathX = undefined;
-    this.pathY = undefined;
+    this.pathX = 200;
+    this.pathY = 200;
     this.pathSize = 300;
-    this.pathAngle = undefined;
+    this.pathAngle = 0;
   }
 
   update() {
+    this.rotatingPath();
     this.selfRotate();
     this.display();
+  }
+
+  rotatingPath() {
+    this.pathAngle -= this.rotatingSpeed;
   }
 
   selfRotate() {
     this.angle -= this.rotatingSpeed;
   }
 
-  rotatingPath() {
-
-  }
-
   display() {
+    //draw a path
+    // push();
+    // ellipse(this.pathX, this.pathY, this.pathSize);
+    // pop();
+
     //display the purple enemy
     push();
-    fill(200, 0, 0);
+    fill(104, 59, 138);
     rectMode(CENTER, CENTER);
-    translate(this.x, this.y);
-    rotate(this.angle);
+    //square follows along the path
+    translate(this.pathX, this.pathY);
+    rotate(this.pathAngle);
+    translate(this.pathSize/2, 0); //translating the "anchor point" back to the square
+    rotate(this.angle); //rotating along itself
     rect(0, 0, this.size);
     pop();
 
-    //draw a path
-    push();
-    // translate(this.x, this.y);
-    rotate(this.angle);
-    rect(0, 0, this.size);
-    pop();
+
   }
 }
