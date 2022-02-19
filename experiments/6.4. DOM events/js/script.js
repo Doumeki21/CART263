2ac3dd487bd59c8198874a929f1c4382c175d188
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-19 15:14:46 -0500

    6.4. 17:30 minutes in

---
+++ experiments/6.4. DOM events/js/script.js
@@ -0,0 +1,32 @@
+// let paragraph = document.getElementById(`paragraph`);
+
+//1. change color once
+// setTimeout(function () {
+//   paragraph.style.color = `#ff0000`;
+// }, 3000);
+
+// //2. blinking animation
+// setInterval(blink, 500);
+// function blink() {
+//   let opacity = paragraph.style.opacity;
+//   if (opacity === `1`) {
+//     paragraph.style.opacity = `0`;
+//   }
+//   else {
+//     paragraph.style.opacity = `1`;
+//   }
+// }
+
+// //3. fade animation
+// let opacity = 1;
+//
+// fadeOut();
+//
+// function fadeOut() {
+//   opacity -= 0.01;
+//   paragraph.style.opacity = opacity;
+//   if (opacity > 0) {
+//     //create a draw loop, until it reaches 0
+//     requestAnimationFrame(fadeOut);
+//   }
+// }
