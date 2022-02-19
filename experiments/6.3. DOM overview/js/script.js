276c2b7d2751a7007e50e16d8d452f27941671f1
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-19 13:27:20 -0500

    6.3 37:30 min in

--- experiments/6.3. DOM overview/js/script.js
+++ experiments/6.3. DOM overview/js/script.js
@@ -1,8 +1,33 @@
-let mainHeading = document.getElementById(`main-heading`);
-let wellSection = document.getElementById(`well-section`);
+// let mainHeading = document.getElementById(`main-heading`);
+// let wellSection = document.getElementById(`well-section`);
+// let pronoun = document.getElementById(`pronoun`);
+//
+// //changing the styling of the text
+// mainHeading.style.color = `#339966`;
+// mainHeading.style.fontSize = `4rem`;
+// wellSection.style.fontSize = `2rem`;
+// mainHeading.style.fontFamily = `Courier, monospace`;
+//
+// pronoun.innerHTML = `<strong>you</strong>`;
+//
+// //changing the textual content
+// mainHeading.innerText = `I FEEL GREAT!`;
+// // if (pronoun.innerText === `we`) {
+// //   pronoun.innerText = `you`;
+// // }
 
-mainHeading.style.color = `#339966`;
-mainHeading.style.fontSize = `4rem`;
-mainHeading.style.fontFamily = `Courier, monospace`;
+let image = document.getElementById(`clown-image`);
+//changing the source attribute to another img!
+image.setAttribute(`src`, `http://loremflickr.com/320/240/clown`);
 
-wellSection.style.fontSize = `2rem`;
+//not exactly an array, IT'S A HTML COLLECTION, but we can treat it like an array
+//array of headers (3)
+let headers = document.getElementsByClassName(`header`);
+for (let i = 0; i < headers.length; i++) {
+  headers[i].style.color = `#00ff33`;
+}
+
+let h2s = document.getElementsByTagName(`h2`);
+for (let i = 0; i < h2s.length; i++) {
+  h2s[i].style.color = `#ff6666`;
+}
