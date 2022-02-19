979b414e858b684804b0b4c5b550e7746c00c807
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-19 18:43:48 -0500

    6.5 11min in

---
+++ experiments/6.5. DOM-form-input/js/script.js
@@ -0,0 +1,13 @@
+let exButton = document.getElementById(`example-button`);
+
+exButton.addEventListener(`click`, function (event) {
+  // alert(`Nice clicking!`);
+  event.target.style.display = `none`;
+})
+
+let textInput = document.getElementById(`example-text-input`);
+let button = document.getElementById(`submit-button`);
+
+button.addEventListener(`click`, function(event) {
+
+});
