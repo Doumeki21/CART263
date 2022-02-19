c08d7de494b36249fec814b451f15ca8f7c9b090
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-19 18:27:24 -0500

    6.4 done

--- experiments/6.4. DOM events/js/script.js
+++ experiments/6.4. DOM events/js/script.js
@@ -30,3 +30,55 @@
 //     requestAnimationFrame(fadeOut);
 //   }
 // }
+
+// //interactive click #1
+// let paragraph = document.getElementById(`paragraph`);
+// //when a click happens, the event function is called
+// paragraph.addEventListener(`click`, function(event) {
+//   paragraph.innerText = `${event.clientX}, ${event.clientY}`;
+// });
+
+//interactive click #2
+// let mainHeading = document.getElementById(`main-heading`);
+// let subHeading = document.getElementById(`sub-heading`);
+//
+// mainHeading.addEventListener(`click`, setRedTextColor);
+// subHeading.addEventListener(`click`, setRedTextColor);
+//
+// function setRedTextColor(event) {
+//   event.target.style.color = `#ff0000`;
+// }
+
+let paragraph = document.getElementById(`paragraph`);
+let originalText = paragraph.innerText;
+
+// //1.mouse events
+// paragraph.addEventListener(`mouseenter`, function(event) {
+//   event.target.innerText = `TOAST IS GREAT!`;
+// });
+//
+// paragraph.addEventListener(`mouseleave`, function(event) {
+//   event.target.innerText = originalText;
+// });
+
+// //2. key Events #1
+// document.addEventListener(`keydown`, function (event) {
+//   // if (event.key === `r`) {
+//   //   paragraph.style.color = `#ff6666`;
+//   // }
+//
+//   paragraph.innerText = paragraph.innerText +event.key;
+// });
+
+//2. key events #2
+document.addEventListener(`keyup`, function(event) {
+  paragraph.style.color = `#ff6666`;
+})
+document.addEventListener(`keydown`, function(event) {
+  paragraph.style.color = `#000000`;
+})
+
+let mainHeading = document.getElementById(`main-heading`);
+window.addEventListener(`offline`, function(event) {
+  mainHeading.innerText = `:(`;
+})
