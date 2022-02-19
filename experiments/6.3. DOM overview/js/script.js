f40f6c20fe8f2a789f4bb4aba077fdd955a68857
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-19 00:56:03 -0500

    6.3 23 min in

---
+++ experiments/6.3. DOM overview/js/script.js
@@ -0,0 +1,8 @@
+let mainHeading = document.getElementById(`main-heading`);
+let wellSection = document.getElementById(`well-section`);
+
+mainHeading.style.color = `#339966`;
+mainHeading.style.fontSize = `4rem`;
+mainHeading.style.fontFamily = `Courier, monospace`;
+
+wellSection.style.fontSize = `2rem`;
