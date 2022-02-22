ca5073a9cd74846a733ede516c09fce470178a57
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-22 00:30:17 -0500

    ACT6: DONE

--- activities/06-haiku-generator/js/script.js
+++ activities/06-haiku-generator/js/script.js
@@ -35,6 +35,45 @@ line1P.innerText = line1;
 line2P.innerText = line2;
 line3P.innerText = line3;
 
+line1P.addEventListener(`click`, lineClicked);
+line2P.addEventListener(`click`, lineClicked);
+line3P.addEventListener(`click`, lineClicked);
+
+function lineClicked(event) {
+  //fadeOut(the element you want to fade, starting opacity)
+  fadeOut(event.target, 1);
+}
+
+//changing the opacity of the element
+function fadeOut(element, opacity) {
+  opacity -= 0.1;
+  //assigning the new value of opacity into the css style
+  element.style.opacity = opacity;
+  if (opacity > 0) {
+    //call this function to prevent a loop of function within itself.
+    requestAnimationFrame(function() {
+      fadeOut(element, opacity);
+    });
+  }
+  else {
+    //generate new line
+    setNewLine(element);
+    //fade in animation. opacity = 0
+    fadeIn(element, opacity);
+  }
+}
+
+function fadeIn(element, opacity) {
+  //adds to opacity
+  opacity += 0.01;
+  element.style.opacity = opacity;
+  if (opacity < 1) {
+    requestAnimationFrame(function() {
+      fadeIn(element, opacity);
+    });
+  }
+}
+
 //(element) = we're looking at the element from the html
 function setNewLine(element) {
   if (element === line1P || element === line3P) {
@@ -45,18 +84,9 @@ function setNewLine(element) {
   }
 }
 
-line1P.addEventListener(`click`, lineClicked);
-line2P.addEventListener(`click`, lineClicked);
-line3P.addEventListener(`click`, lineClicked);
-
-function lineClicked(event) {
-  setNewLine(event.target);
-}
-
-
-
 function random(array) {
   // math.floor = gives a whole number
+  //randomize the index using this specfic (commom) formula.
   let index = Math.floor(Math.random() * array.length);
   return array[index];
 }
