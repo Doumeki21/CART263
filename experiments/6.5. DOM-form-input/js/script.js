5ef77c710a757bba4ed511857290d340ea97afa8
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-21 23:05:45 -0500

    6.5 DONE

--- experiments/6.5. DOM-form-input/js/script.js
+++ experiments/6.5. DOM-form-input/js/script.js
@@ -1,13 +1,42 @@
-let exButton = document.getElementById(`example-button`);
+// //EXAMPLE 1
+// let exButton = document.getElementById(`example-button`);
+// exButton.addEventListener(`click`, function (event) {
+//   // alert(`Nice clicking!`);
+//   event.target.style.display = `none`;
+// })
+//
+// //EXAMPLE 2
+// let textInput = document.getElementById(`text-input`);
+// let button = document.getElementById(`submit-button`);
+// //mouse click submit
+// button.addEventListener(`click`, function(event) {
+//   let input = textInput.value;
+//   alert(input);
+// });
+// //hit enter key
+// textInput.addEventListener(`keydown`, function(event) {
+//   if (event.keyCode === 13) {
+//     let input = textInput.value;
+//     alert(input);
+//   }
+// })
 
-exButton.addEventListener(`click`, function (event) {
-  // alert(`Nice clicking!`);
-  event.target.style.display = `none`;
-})
+// //EXAMPLE 3
+// let slider = document.getElementById(`slider`);
+// // 1. add a button to show the range
+// let button = document.getElementById(`check-button`)
+// button.addEventListener(`click`, function(event) {
+//   let value = slider.value;
+//   alert(value);
+// });
+//
+// //2.
+// slider.addEventListener(`change`, function(event) {
+//   alert(slider.value);
+// })
 
-let textInput = document.getElementById(`example-text-input`);
-let button = document.getElementById(`submit-button`);
-
-button.addEventListener(`click`, function(event) {
-
+let picker = document.getElementById(`color-picker`);
+picker.addEventListener(`change`, function(event) {
+  let color = picker.value;
+  document.body.style.backgroundColor = color;
 });
