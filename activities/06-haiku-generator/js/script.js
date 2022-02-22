f237b2d66a3bdc35236064a4a1cb1c9b6eaa94ed
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-02-21 23:58:18 -0500

    ACT6: haiku works

--- activities/06-haiku-generator/js/script.js
+++ activities/06-haiku-generator/js/script.js
@@ -1,11 +1,62 @@
 /**
-Title of Project
+ACTIVITY 06: Haiku generator
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Generates a random haiku.
 */
 
 "use strict";
 
-// Code goes here
+let fiveSyllableLines = [
+  `O, to be a tree`,
+  `The cat does not know`,
+  `We are all forests`,
+  `You have done your best`,
+  `They are all gone now`,
+];
+
+let sevenSyllableLines = [
+  `Say the things left unsaid`,
+  `Never believe the wind's lies`,
+  `The autumn stretches its legs`,
+  `Nothing can satisfy you`,
+  `They will not come back again`,
+];
+
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+let line1P = document.getElementById(`line-1`);
+let line2P = document.getElementById(`line-2`);
+let line3P = document.getElementById(`line-3`);
+
+line1P.innerText = line1;
+line2P.innerText = line2;
+line3P.innerText = line3;
+
+//(element) = we're looking at the element from the html
+function setNewLine(element) {
+  if (element === line1P || element === line3P) {
+    element.innerText = random(fiveSyllableLines);
+  }
+  else if (element === line2P) {
+    element.innerText = random(sevenSyllableLines);
+  }
+}
+
+line1P.addEventListener(`click`, lineClicked);
+line2P.addEventListener(`click`, lineClicked);
+line3P.addEventListener(`click`, lineClicked);
+
+function lineClicked(event) {
+  setNewLine(event.target);
+}
+
+
+
+function random(array) {
+  // math.floor = gives a whole number
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
