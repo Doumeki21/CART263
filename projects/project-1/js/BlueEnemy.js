7b7cbbc46bd5207ef9aea36e5bb93ff5ae43eff1
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-03-02 17:43:33 -0500

    p1: finished blue enemy comments

--- projects/project-1/js/BlueEnemy.js
+++ projects/project-1/js/BlueEnemy.js
@@ -1,4 +1,7 @@
+//Blue RedEnemy - an extension to the Enemies class
+//Randomly appears and drops fast to the bottom of the canvas
 class BlueEnemy extends Enemies{
+  //calls the super constructor and adds properties for it to display
   constructor(x, y) {
     super(x, y);
     this.size = undefined;
@@ -9,18 +12,21 @@ class BlueEnemy extends Enemies{
     this.ay = 2;
     this.acceleration = 0.5;
     this.active = false;
-
     this.activate();
   }
 
+//update()
+//Calls the super update() and all the functions/ events within this class.
   update() {
+    super.update();
     this.display();
     this.checkActive();
     this.checkAppearing();
   }
 
-// enemy is invisible, randomizes size, and location until it's visible again.
+//checkActive()
   checkActive() {
+    // enemy is invisible, randomizes size, and location until it's visible again.
     if (!this.active) {
       this.alpha = 0;
       this.x = random(0, width);
@@ -28,40 +34,27 @@ class BlueEnemy extends Enemies{
       this.vy = this.initialVy;
       this.size = random(100, 500);
     }
+    //once it hits the bottom of the canvas, blue enemy disappears.
     if (this.y >= height) {
       this.active = false;
     }
   }
 
+//checkAppearing()
   checkAppearing() {
     if (this.active) {
-      //fade in
+      //starts by fading in
       this.alpha += this.alphaSpeed;
-
       //add movement (by adding velocity to the coordinates)
       this.y += this.vy;
       //Add the acceleration to the velocity
       this.vy += this.ay;
-
-      //constrain the enemy within a reasonable area of the canvas
-      // this.vy = constrain(this.vy, 0, 10);
+      //constrain the enemy within the canvas
       this.y = constrain(this.y, 0, height);
     }
   }
 
-  moveEnemy() {
-    //add moevement (by adding velocity to the coordinates)
-    this.x += this.vx;
-    this.y += this.vy;
-    //Add the acceleration to the velocity
-    this.vx += this.ax;
-    this.vy += this.ay;
-
-    //constrain the enemy within a reasonable area of the canvas
-    this.vx = constrain(this.vx, -5, 5);
-    this.vy = constrain(this.vy, -5, 5);
-  }
-
+//display the blue enemy if it's active
   display() {
     if (this.active) {
       push();
