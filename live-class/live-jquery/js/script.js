e0877aca1046c427d703f28f83dbc999772f83b4
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-03-08 14:53:09 -0500

    live jquery: error

--- live-class/live-jquery/js/script.js
+++ live-class/live-jquery/js/script.js
@@ -1,75 +1,38 @@
-/**
-live: review
-
--Constants for strings
--Parameters
--First class functions
--SetTimeout() show text for specific amount of time
-*/
-
 "use strict";
 
-//Constants for strings
-// const IMAGE_PATH = `assets/images`;
-// const NUM_CLOWN_IMAGES = 3;
-//
-// let clownImages = [];
-//
-// function preload() {
-//   clownImages[0] = loadImage(`${IMAGE_PATH}/clown0.png`);
-//   clownImages[1] = loadImage(`${IMAGE_PATH}/clown1.png`);
-//   clownImages[2] = loadImage(`${IMAGE_PATH}/clown2.png`);
-//
-//   for (let i=0; i<NUM_CLOWN_IMAGES; i++) {
-//     clownImages[i] = loadImage(`${IMAGE_PATH}/clown${i}.png`);
-//   }
-// }
-//
+// let canvas = undefined;
 //
 // function setup() {
-//   createCanvas(500, 500);
-// }
+//   canvas = createCanvas(windowWidth, windowHeight);
+//   canvas.id(`#p5-canvas`);
 //
+//   $(document).on(`click`, function() {
+//     $(`#p5-canvas`).hide();
+//   });
+// }
 //
 // function draw() {
 //   background(0);
 //
-//   let clownImage = random(clownImages);
-//   image(clownImage, 0, 0);
+//   rectMode(CENTER);
+//   rect(mouseX, mouseY, 100, 100);
 // }
 
-//SetTimeout
+let mouseX = 0;
+let mouseY = 0;
 
-let message = {
-  text: `NOW YOU SEE ME`,
-  visible: true,
-  hideDelay: 200,
-};
+$(document).on(`mousemove`, function(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+})
 
-function setup() {
-  createCanvas(500, 500);
+window.requestAnimationFrame(animateCircle);
+
+function(animateCircle) {
+  $(`#circle`).css({
+    top: `$(mouseY)px`,
+    left: `$(mouseX)px`,
+  })
+
+  window.requestAnimationFrame
 }
-
-function draw() {
-  background(0);
-
-  //Display msg (or not)
-  if (message.visible) {
-    push();
-    fill(255);
-    textSize(24);
-    text(message.text, 100, 100);
-    pop();
-  }
-}
-
-function mousePressed() {
-  setTimeout(function () {
-    message.visible = false;
-  }, message.hideDelay);
-  // hideMessage();
-}
-
-// function hideMessage() {
-//   message.visible = false;
-// }
