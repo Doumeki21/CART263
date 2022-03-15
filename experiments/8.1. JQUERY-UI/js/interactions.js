d17f323280190eeba1ff9404f002a2b720d6269c
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-03-15 00:52:58 -0400

    8.3 done

--- experiments/8.1. JQUERY-UI/js/interactions.js
+++ experiments/8.1. JQUERY-UI/js/interactions.js
@@ -1,21 +1,34 @@
-$(`#prisoner`).draggable({
-  // axis: `x`,
-  containment: `#prison`,
-  start: function(event, ui) {
-    //this refers to the prisoner
-    $(this).css(`text-decoration`, `underline`);
-  },
-  stop: function (event, ui) {
-    $(this).css(`text-decoration`, `none`);
-  },
+$(`#prisoner`).effect({
+  effect: `shake`,
+  duration: 1000,
+  distance: 7,
+  times: 10,
+  complete: makePrisonerDraggable
 });
+
+// $(`#escape-tunnnel`).droppable({
+//   drop: function(event, ui) {
+//     ui.draggable.remove();
+//     $(this).hide({
+//       effect: `blind`,
+//       duration: 500
+//     });
+//   },
+// });
+
+function makePrisonerDraggable() {
+  $(`#prisoner`).draggable({
+    // axis: `x`,
+    containment: `#prison`,
+    start: function(event, ui) {
+      $(this).addClass(`prisoner-dragging`, 750);
+    },
+    stop: function (event, ui) {
+      $(this).removeClass(`prisoner-dragging`, 750);
+    },
+  });
+}
 
 // setTimeout(function() {
 //   $(`#prisoner`).draggable(`disable`);
 // }, 500);
-
-$(`#escape-tunnnel`).droppable({
-  drop: function(event, ui) {
-    $(`#prisoner`).remove();
-  },
-});
