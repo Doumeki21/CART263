e9dead9ebb00c69eb54bfe603d82f4ffd3718619
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-03-15 14:10:37 -0400

    live jquery-ui: added 2 prisoners (ui.draggable)

--- live-class/live-jquery-ui/js/script.js
+++ live-class/live-jquery-ui/js/script.js
@@ -8,30 +8,46 @@ author, and this description to match your project!
 
 "use strict";
 
-$(`#solved-dialog`).dialog({
-  autoOpen: false,
-  buttons: {
-    "I know": function() {
-      $(this).dialog(`close`);
-    }
-  }
+// Prisoner shakes with rage at the start
+$(`.prisoner`).effect({
+  effect: `shake`,
+  duration: 1000,
+  times: 10,
+  distance: 5,
+  complete: makePrisonerDraggable
 });
 
-//use `one` instead of `on` event so we don't have to `off` event for `mousover` after, the `one` event does it once.
-$(`.secret`).one(`mouseover`, function(event) {
-  //this >> for only the specific .secret.
-  $(this).addClass(`found`, 500);
-  $(this).draggable({
-    helper: `clone`,
-  });
-});
-
-$(`#answer`).droppable({
+// Escape tunnel is droppable
+$(`#escape-tunnel`).droppable({
+  // Elements dropped on escape tunnel are removed from the page
   drop: function(event, ui) {
-    let letter = ui.draggable.text();
-    $(this).append(letter);
-    //once appended >> disable draggable
-    ui.draggable.draggable(`disable`);
-    ui.draggable.removeClass(`found`);
+    //ui.draggable = specific prisoner (object) if there are more.
+    //if three is a class of prisoner- using `this` will effect all prisoners.
+    ui.draggable.remove();
+    $(this).hide({
+      effect: `blind`,
+      duration: 500
+    });
   }
 });
+
+/**
+Makes the prisoner draggable
+*/
+function makePrisonerDraggable() {
+  $(this).draggable();
+  // Prisoner is draggable
+  $(this).draggable({
+    // Prisoner cannot be dragged out of the prison
+    containment: `#prison`,
+    // Prisoner gets an underline and turns blue when dragging starts
+    start: function(event, ui) {
+      $(this).addClass(`prisoner-dragging`, 1000);
+    },
+    // Prisoner loses underline and turns black when dragging stops
+    stop: function(event, ui) {
+      // NEW! Animated class removal
+      $(this).removeClass(`prisoner-dragging`, 1000);
+    }
+  });
+}
