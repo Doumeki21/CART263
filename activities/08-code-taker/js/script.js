5c57a6910ca736dbb19f4a5cd14fdac0f223b84b
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-03-15 14:25:45 -0400

    ACT8: done

--- activities/08-code-taker/js/script.js
+++ activities/08-code-taker/js/script.js
@@ -11,14 +11,14 @@ author, and this description to match your project!
 $(`#solved-dialog`).dialog({
   autoOpen: false,
   buttons: {
-    "I know": function() {
+    "I know": function () {
       $(this).dialog(`close`);
-    }
-  }
+    },
+  },
 });
 
 //use `one` instead of `on` event so we don't have to `off` event for `mousover` after, the `one` event does it once.
-$(`.secret`).one(`mouseover`, function(event) {
+$(`.secret`).one(`mouseover`, function (event) {
   //this >> for only the specific .secret.
   $(this).addClass(`found`, 500);
   $(this).draggable({
@@ -27,11 +27,15 @@ $(`.secret`).one(`mouseover`, function(event) {
 });
 
 $(`#answer`).droppable({
-  drop: function(event, ui) {
+  drop: function (event, ui) {
     let letter = ui.draggable.text();
     $(this).append(letter);
     //once appended >> disable draggable
     ui.draggable.draggable(`disable`);
     ui.draggable.removeClass(`found`);
-  }
+    //check answer
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
 });
