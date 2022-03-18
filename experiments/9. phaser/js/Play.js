a2a58c5c7732baa19d3250a2a800d478f5de4768
OlenkaYuen <84202232+Doumeki21@users.noreply.github.com>
2022-03-18 18:29:59 -0400

    phaser 3: video 4 done

--- experiments/9. phaser/js/Play.js
+++ experiments/9. phaser/js/Play.js
@@ -6,17 +6,45 @@ class Play extends Phaser.Scene {
   }
 
   create() {
-    this.wall = this.add.image(100, 100, `wall`);
+    //the wall
+    this.wall = this.physics.add.image(100, 100, `wall`);
     this.wall.setTint(0xdd3333);
 
-    this.avatar = this.add.sprite(200, 200, `avatar`);
-
+    //the player
+    this.avatar = this.physics.add.sprite(200, 200, `avatar`);
+    //animation settings
     this.createAnimations();
+    // this.avatar.setVelocityY(100);
     this.avatar.play(`avatar-idle`);
+    this.avatar.setCollideWorldBounds(true);
+
+    //the handleinput
+    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
+    this.avatar.setVelocity(0);
 
+    if(this.cursors.left.isDown) {
+      this.avatar.setVelocityX(-300);
+    }
+    else if (this.cursors.right.isDown) {
+      this.avatar.setVelocityX(300);
+    }
+
+    if(this.cursors.up.isDown) {
+      this.avatar.setVelocityY(-300);
+    }
+    else if (this.cursors.down.isDown) {
+      this.avatar.setVelocityY(300);
+    }
+
+    if (this.avatar.body.velocity.x !== 0 || this.avatar.body.velocity.y !== 0) {
+      this.avatar.play(`avatar-moving`, true); //true >> if it's already playing don't restart the animation
+    }
+    else {
+      this.avatar.play(`avatar-idle`);
+    }
   }
 
   createAnimations() {
